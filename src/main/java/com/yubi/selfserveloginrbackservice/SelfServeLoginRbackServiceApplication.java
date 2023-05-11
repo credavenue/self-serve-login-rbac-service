2fb2ed9b68b0f040a46aa2283d5fb510a24abbd7
Mangi Lal <mangi.lal@go-yubi.com>
2023-05-11 18:29:46 +0530

    first commit

---
+++ src/main/java/com/yubi/selfserveloginrbackservice/SelfServeLoginRbackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yubi.selfserveloginrbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfServeLoginRbackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfServeLoginRbackServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yubi/selfserveloginrbackservice/SelfServeLoginRbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yubi.selfserveloginrbackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfServeLoginRbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
