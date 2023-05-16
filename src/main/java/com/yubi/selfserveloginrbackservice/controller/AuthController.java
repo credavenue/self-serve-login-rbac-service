66a7bf73d533e4dfe0860a6a3a25252f3a4b1591
Mangi Lal <mangi.lal@go-yubi.com>
2023-05-16 15:09:31 +0530

    added returning redis data back when key exists

--- src/main/java/com/yubi/selfserveloginrbackservice/controller/AuthController.java
+++ src/main/java/com/yubi/selfserveloginrbackservice/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.yubi.selfserveloginrbackservice.controller;
 import com.yubi.selfserveloginrbackservice.model.UserInfo;
 import java.net.URI;
 import java.net.URISyntaxException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import redis.clients.jedis.Jedis;
 
 @RestController
+@Slf4j
 public class AuthController {
 
     @Value("${authority}")
@@ -33,8 +36,20 @@ public class AuthController {
         @RequestHeader String authorization,
         @RequestHeader String mfaToken,
         @RequestHeader String currentEntityId,
-        @RequestHeader String currentGroup) throws URISyntaxException {
+        @RequestHeader String currentGroup) {
         RestTemplate restTemplate = new RestTemplate();
+        String caUserId = userInfo.getCaUserId();
+
+        // Check if the data exists in Redis
+        String cachedData;
+        try (Jedis jedis = new Jedis(redisHostname)) {
+            cachedData = jedis.get(caUserId);
+        }
+
+        if (cachedData != null) {
+            log.info("Cached data found for caUserId: {}", caUserId);
+            return ResponseEntity.ok().body(cachedData);
+        }
 
         // Set query parameters
         String productId = userInfo.getProductId();
@@ -51,23 +66,39 @@ public class AuthController {
         headers.add("mfa-token", mfaToken);
         headers.add("origin", origin);
 
-        // Build the request URI with query parameters
-        URI uri = new URI("https://auth-qa-api.go-yubi.in/users/" + currentEntityId
-            + "/all_permissions_web?product_id=" + productId + "&entity_id=" + entityId + "&group_id=" + groupId);
+        URI uri = null;
+        ResponseEntity<Object> responseEntity = null;
 
-        // Create the request entity with method, headers, and body
-        RequestEntity<Object> requestEntity = new RequestEntity<>(headers, HttpMethod.GET, uri);
+        try {
+            uri = new URI("https://auth-qa-api.go-yubi.in/users/" + "6360f976d964090043b64dde"
+                + "/all_permissions_web?product_id=" + productId + "&entity_id=" + entityId
+                + "&group_id=" + groupId);
+            log.info("Sending request to: {}", uri);
+            RequestEntity<Object> requestEntity = new RequestEntity<>(headers, HttpMethod.GET, uri);
+            // Send the HTTP GET request
+            responseEntity = restTemplate.exchange(requestEntity, Object.class);
 
-        // Send the HTTP GET request
-        ResponseEntity<Object> responseEntity = restTemplate.exchange(requestEntity, Object.class);
+            if (responseEntity != null && responseEntity.getBody() != null) {
+                // Store the response in Redis
+                try (Jedis jedis = new Jedis(redisHostname)) {
+                    int expiryTimeInSeconds = 3600;
+                    jedis.setex(caUserId, expiryTimeInSeconds, responseEntity.getBody().toString());
+                }
+                log.info("Response stored in Redis for caUserId: {}", caUserId);
+            } else {
+                log.warn("Response entity or its body is null");
+            }
 
-        // Store the response in Redis
-        String caUserId = userInfo.getCaUserId();
-        Jedis jedis = new Jedis(redisHostname); // Replace with the Redis server address if needed
-        int expiryTimeInSeconds = 3600; // Set the expiry time (e.g., 1 hour)
-        jedis.setex(caUserId, expiryTimeInSeconds, responseEntity.getBody().toString());
-        jedis.close();
+        } catch (URISyntaxException ex) {
+            log.error("Invalid URI syntax: {}", ex.getMessage());
+        } catch (HttpStatusCodeException ex) {
+            log.error("HTTP request failed with status code {}: {}", ex.getStatusCode(), ex.getMessage());
+        } catch (Exception ex) {
+            log.error("Exception occurred while sending request to platform: {}", ex.getMessage());
+        }
+
         return responseEntity;
     }
+
 
 }
